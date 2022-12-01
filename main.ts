f3c4c66c97ec9b3f1af8bed8db98176739053d82
Alexander-James7 <113034143+Alexander-James7@users.noreply.github.com>
2022-12-01 09:48:41 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(30)
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(180)
+})
+basic.showIcon(IconNames.Heart)
